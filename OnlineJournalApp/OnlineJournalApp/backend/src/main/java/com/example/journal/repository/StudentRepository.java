6b1da338a3499b53d18d3af6d7defc0d489002b1
Sanstorm15 <taraskovtun2004@gmail.com>
2025-06-12 10:49:56 +0300

    Delete old OnlineJournalApp/OnlineJournalApp directory

--- OnlineJournalApp/OnlineJournalApp/backend/src/main/java/com/example/journal/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.journal.repository;
-
-import com.example.journal.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
